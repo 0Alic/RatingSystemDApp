223eeef7870e5926bb9b675c5abe5bdb98dce1f7
0Alic <andrealisi.12lj@gmail.com>
2019-04-01 16:36:57 +0200

    Moved in DeployModal file

--- src/SearchBar.js
+++ src/SearchBar.js
@@ -4,88 +4,13 @@ import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 import Button from 'react-bootstrap/Button';
 import Navbar from 'react-bootstrap/Navbar';
-import Modal from 'react-bootstrap/Modal'
+// import Modal from 'react-bootstrap/Modal'
 // import BootstrapTable from 'react-bootstrap-table-next';
 // import AddRateForm from './AddRateForm.js';
 import Nav from 'react-bootstrap/Nav';
 import ItemModal from './ItemModal.js';
 import UserModal from './UserModal.js';
-
-
-/**
- * This component shows the form to upload a new item
- * 
- */
-
-class DeployModal extends React.Component {
-
-    constructor(props) {
-        super(props);
-        this.i = 0; // to Suppress warning
-    }
-
-    async createItem(e) {
-
-        e.preventDefault();
-
-        const title = e.currentTarget.itemTitle.value;
-        const user = this.props.user;
-        const web3 = this.props.web3;
-        const account = this.props.account;
-
-        if(title === "") {
-            alert("Blank game title");
-            return;
-        }
-
-        if(title.length > 32) {
-            alert("Game title too long");
-            return;
-        }
-
-        // Deploy Item
-        await user.createItem(web3.utils.fromUtf8(title), {from: account});
-    }
-
-    render() {
-
-        if(true) {
-
-            return (
-                <Modal
-                    onHide={this.props.onHide}
-                    show={this.props.show}
-                    size="lg"
-                    aria-labelledby="contained-modal-title-vcenter"
-                    centered >
-                    
-                    <Modal.Header closeButton>
-                        <Modal.Title id="contained-modal-title-vcenter">
-                            Add a new board game to this list
-                        </Modal.Title>
-                    </Modal.Header>
-                    <Modal.Body>
-                        <h4>Choose a name</h4>
-                        <Form onSubmit={e => this.createItem(e)}>
-                            <Form.Control type="text" id="itemTitle" placeholder="Write title here" />
-                            <Button type="submit">Publish this game</Button>
-                        </Form>
-                    </Modal.Body>
-                    <Modal.Footer>
-                        <Button onClick={this.props.onHide}>Close</Button>
-                    </Modal.Footer>
-                </Modal>
-            );
-        }
-        else
-            return (
-                <Modal onHide={this.props.onHide}
-                        show={this.props.show}>
-                    Information not found
-                </Modal>
-            ); 
-        }
-}
+import DeployModal from './DeployModal.js';
 
 
 /**
